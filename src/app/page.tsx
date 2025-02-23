a6a3cc841aaddb8b3ff2d35353a395eea065dcff
alex-curtin <alexhcurtin@gmail.com>
2025-02-23 17:19:33 -0500

    update text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start gap-8 p-24">
-      <h1 className="text-2xl">Hello World</h1>
+      <h1 className="text-2xl text-purple-200">Hello World</h1>
       <div className="max-w-[80%] text-neutral-300">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
         corporis et est mollitia sint asperiores repudiandae, quos error vero
