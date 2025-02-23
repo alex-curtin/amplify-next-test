d02d05ef9783aa754c7c8dcfb838b2c0dbe255b8
alex-curtin <alexhcurtin@gmail.com>
2025-02-23 17:28:32 -0500

    re-add second change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl text-purple-200">Hello World</h1>
+    <main className="flex min-h-screen flex-col items-center justify-start gap-8 p-24">
+      <h1 className="text-2xl">Hello World</h1>
+      <div className="max-w-[80%] text-neutral-300">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+        corporis et est mollitia sint asperiores repudiandae, quos error vero
+        nulla dolores quia molestiae itaque nesciunt reiciendis cum totam!
+        Obcaecati, quis error sequi magni odio exercitationem saepe ducimus,
+        praesentium eaque officia deleniti ad quisquam iure, doloremque totam
+        earum. Delectus, illo atque!
+      </div>
     </main>
   );
 }
